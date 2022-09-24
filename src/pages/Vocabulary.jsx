e84a9bf6e9e7de75bbf6f77df00bbf8dcfc9b684
K1ttK1tty <vlad.petuxov.2018@bk.ru>
2022-09-24 13:16:31 +0700

    bug fixed

--- src/pages/Vocabulary.jsx
+++ src/pages/Vocabulary.jsx
@@ -99,14 +99,17 @@ const Vocabulary = function () {
 
 
     function addNewTheme(selectOptions, newTheme) {
-        let opt = [...selectOptions, newTheme];
-        opt.filter(theme => (theme !== newTheme))
-        console.log(selectOptions)
-        console.log(newTheme)
-        // selectOptions.filter(theme => (console.log(theme )))
-        
-        setSelectOptions([...opt])
-     
+        let opt = [...selectOptions];
+        let a = 0;
+        for (let index = 0; index < opt.length; index++) {
+            const element = opt[index];
+            if (element == newTheme) {
+                a=1;
+            }
+        }
+        if (!a) {
+            setSelectOptions([...selectOptions, newTheme]) 
+        } 
     }
 
     function removeCard(cardClick) {
