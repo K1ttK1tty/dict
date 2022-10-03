d8f68a28057e036418a7fe19fcc8aee6128c4db3
K1ttK1tty <vlad.petuxov.2018@bk.ru>
2022-10-03 13:44:02 +0700

    game 2

---
+++ src/components/UI/CardsRandom/CardsRandom.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import CardRand from '../CardRand/CardRand'
+const CardsRandom = function ({ cardsGame }) {
+
+
+
+
+
+
+
+    return (
+        <div>
+
+            {
+                cardsGame.map((card,index) => <CardRand key={index+card.word} card={card}/>)
+
+
+            }
+
+
+        </div>
+    )
+};
+export default CardsRandom;
--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -1,42 +1,67 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import InputRequire from '../components/UI/InputRequire/InputRequire'
 import Gamescss from '../styles/Games.css'
 import vocabularyCss from '../styles/Vocabulary.css'
 import BtnAddCard from '../components/UI/BtnAddCard/BtnAddCard';
+import CardsRandom from '../components/UI/CardsRandom/CardsRandom'
 const Games = function ({ Cards }) {
     const [inputReq, setInputReq] = useState('');
     const [numb, setNumb] = useState();
-
-
-
+    let cardsGame = [];
     function getRandomInt(max) {
         return Math.floor(Math.random() * max);
     }
     //5
+
+
     function generate(e) {
         e.preventDefault();
         setNumb(inputReq);
+        setInputReq('');
     };
 
+   
+
+    const array = useMemo(() => {
+        for (let index = 0; index < numb; index++) {
+            cardsGame.push(Cards[getRandomInt(Cards.length)])
+        }
+
+        return cardsGame
+
+    }, [numb])
 
 
 
 
-    // for (let index = 0; index < 3; index++) {
-    //     console.log(Cards[getRandomInt(Cards.length)])
+
+
+
+    // export const useCards = (Cards,searchWord, chooseTheme) => {
+    //     const selectedThemes = useSelectedThemes(Cards, chooseTheme)
+
+    //     const selectedAndSearchedWord = useMemo(() => {
+    //         return selectedThemes.filter(card => card.theme.toLowerCase().includes(searchWord.toLowerCase())); // перепутаны местами
+    //     }, [selectedThemes, searchWord])
+
+    //     return selectedAndSearchedWord
     // }
-
     return (
         <div className='gameWrap'>
             <div className="CardsField">
                 <div className='wrap'>
                     <div style={{ width: '50%', margin: '0px auto' }}>
-                        
+
                         <form>
                             Введите количество слов: <InputRequire style={{ marginRight: '8px' }} inputReq={inputReq} setInputReq={setInputReq} />
                             <BtnAddCard onClick={generate} type='submit'>Сгенериовать</BtnAddCard>
                         </form>
+
+                        <CardsRandom cardsGame={cardsGame} />
+
+
+
 
                     </div>
                 </div>
