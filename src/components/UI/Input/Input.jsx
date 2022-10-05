0b69eb86e0d4735ca9ca8d7c1266a7eec26fb72e
K1ttK1tty <vlad.petuxov.2018@bk.ru>
2022-10-05 15:38:53 +0700

    game half

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -3,7 +3,7 @@ import cl from '../Input/Input.module.css'
 const Input = function ({ input, setInput,}) {
 
     return (
-        <input onChange={e => setInput(e.target.value)} className={[cl.input,'inptReq'].join(' ')}></input>
+        <input onChange={e => setInput(e.target.value)} className={[cl.input,'inptReq ',].join(' ')}></input>
     )
 };
 export default Input;
